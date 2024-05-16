9a6ca2b4c01231ec19ce0cbac9d1f8aacc41d11b
zein <y.zeinullayev@ecc.kz>
2024-05-16 20:28:51 +0500

    first commit

---
+++ src/main/java/kz/olzhas/event_mvc/EventMvcApplication.java
@@ -0,0 +1,13 @@
+package kz.olzhas.event_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/olzhas/event_mvc/EventMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.olzhas.event_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
